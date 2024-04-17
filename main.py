4311fc385f9e5e3fd96c18db0be8180e57ae6785
Jakub Kosnar <jakubkosnar@seznam.cz>
2024-04-17 17:55:26 +0200

    test - vytvoření tabulky

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import psycopg2
+
+connection = psycopg2.connect(
+        dbname='student',
+        user='postgres',
+        password='admin',
+        host='localhost',
+        port='5432'
+    )
+
+
+cur = connection.cursor()
+cur.execute('''CREATE TABLE teacher(
+            ID SERIAL,
+            NAME TEXT,
+            AGE INT,
+            ADDRESS TEXT
+    )''')
+connection.commit()
+connection.close()
