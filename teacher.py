a1ee5aab2da31264e2d6f32f6eecb9be4a666edd
Jakub Kosnar <jakubkosnar@seznam.cz>
2024-04-19 16:53:59 +0200

    Vytvořen obecný label + name section

--- teacher.py
+++ teacher.py
@@ -6,6 +6,15 @@ root.title("Škola a datáze")
 root.geometry("300x280")
 root.resizable(False,False)
 
+# labels a entries
+label_general = Label(root, text="Add data")
+label_general.grid(row=0, column=1)
 
+# name section
+label_name = Label(root, text="Name: ")
+label_name.grid(row=1, column=0)
+
+entry_name = Entry(root)
+entry_name.grid(row=1, column=1)
 
 root.mainloop()
