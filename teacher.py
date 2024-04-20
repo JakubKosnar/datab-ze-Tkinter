5cc2fcd8a8a2647962f526bb763f6cce5054524a
Jakub Kosnar <jakubkosnar@seznam.cz>
2024-04-20 23:50:24 +0200

    přidání sekce pro vyhledávání podle id

--- teacher.py
+++ teacher.py
@@ -24,6 +24,35 @@ def insert_data(name, age, address):
     connection.close()
 
 
+def search_id(id):
+    
+    connection = psycopg2.connect(
+                dbname='student',
+                user='postgres',
+                password='admin',
+                host='localhost',
+                port='5432'
+            )
+    
+    cur = connection.cursor()
+    query = ('''SELECT * FROM teacher 
+                WHERE id = %s''')
+    cur.execute(query, (id))
+    searched_teacher = cur.fetchone()
+    display_search(searched_teacher)
+    connection.commit()
+    connection.close()
+
+    
+def display_search(data):
+    listbox = Listbox(root, width=20, height=3)
+    listbox.grid(row=8, column=1)
+    listbox.insert(0,("jméno:",data[1]))
+    listbox.insert(1,("věk:",data[2]))
+    listbox.insert(2,("adresa:",data[3]))
+
+
+    
 # labels a entries
 label_general = Label(root, text="Add data")
 label_general.grid(row=0, column=1)
@@ -52,6 +81,21 @@ entry_address.grid(row=3, column=1)
 # button
 button = Button(root, text="Add", command=lambda:insert_data(entry_name.get(), entry_age.get(), entry_address.get()))
 button.grid(row=4, column=1)
+
+## search section
+# general label
+label_search = Label(root, text=" Search data")
+label_search.grid(row=5, column=1)
+
+label_id = Label(root, text="Search by id: ")
+label_id.grid(row=6, column=0)
+
+entry_id=Entry()
+entry_id.grid(row=6, column=1)
+
+button_search = Button(root, text="search", command=lambda:search_id(entry_id.get()))
+button_search.grid(row=6, column=2)
+
 
 
 root.mainloop()
