a47c45e8a286c6117e4a0ffe05fe1cab6bc9ab9a
Jakub Kosnar <jakubkosnar@seznam.cz>
2024-04-23 09:28:37 +0200

    přidání scrollbar a tlačítka show database

--- teacher.py
+++ teacher.py
@@ -3,7 +3,7 @@ import psycopg2
 
 root = Tk()
 root.title("Škola a datáze")
-root.geometry("300x280")
+root.geometry("330x280")
 root.resizable(False,False)
 
 # functions
@@ -45,14 +45,18 @@ def search_id(id):
     cur.execute(query, (id,))
     searched_teacher = cur.fetchone()
     display_search(searched_teacher)
+    
     connection.commit()
     connection.close()
 
     
 def display_search(data):
-    listbox = Listbox(root, width=20, height=4)
-    listbox.grid(row=8, column=1)
-    listbox.insert(0,("id:",data[0]), ("name:",data[1]), ("age:",data[2]), ("address:",data[3]))
+    listbox = Listbox(root, width=20, height=7)
+    listbox.grid(row=7, column=1)
+    if data:
+        listbox.insert(0,("id:",data[0]), ("name:",data[1]), ("age:",data[2]), ("address:",data[3]))
+    else:
+        listbox.insert(0,"ID nenalezeno")
 
 def display_all():
     connection = psycopg2.connect(
@@ -67,19 +71,33 @@ def display_all():
     query = ('''SELECT * FROM teacher''')
     cur.execute(query)
     all_teacher = cur.fetchall()
-    listbox = Listbox(root, width=20, height=4)
-    listbox.grid(row=8, column=1)
+    listbox = Listbox(root, width=20, height=7)
+    listbox.grid(row=7, column=1)
+
+    # vytvoření scrollbar widgetu
+    scrollbar = Scrollbar(root)
+    # umístění scrollbar v okně
+    scrollbar.grid(row=7, column=2, sticky="nsw")
+
+    # připojení Scrollbar k listboxu
+    # scrollbar reaguje na posouvání v listboxu
+    listbox.config(yscrollcommand=scrollbar.set)
+    # listbox reaguje na posouvání scrollbarem
+    scrollbar.config(command=listbox.yview)
 
     for teacher in all_teacher:
         listbox.insert(END,teacher)
 
 def error_id():
-    error = Tk()
-    error.title("Chyba")
-    
-    label_error = Label(error, text="enter ID")
-    label_error.grid(row=0, column=0)
+     listbox = Listbox(root, width=20, height=7)
+     listbox.grid(row=7, column=1)
+     listbox.insert(0,"zadejte ID")
 
+# def delete_from_db(to_delete):
+#     connection = psycopg2.connect(
+#         dbname="student",
+#         user="postgres"
+#     )
     
 # labels a entries
 label_general = Label(root, text="Add data")
@@ -122,7 +140,10 @@ entry_id=Entry()
 entry_id.grid(row=6, column=1)
 
 button_search = Button(root, text="search", command=lambda:search_id(entry_id.get()) if entry_id.get().strip() else error_id())
-button_search.grid(row=6, column=2)
+button_search.grid(row=6, column=3)
+
+button_showall = Button(root, text="show database", command=display_all, height=2, width=7, wraplength=60)
+button_showall.grid(row=7, column=3, sticky="e")
 
 
 display_all()
