61f37f2a996fcdd7779bcf9dad5e823ede8a08bc
Jakub Kosnar <jakubkosnar@seznam.cz>
2024-04-25 10:40:28 +0200

    přidáno talačítko na mazání z db + opravy chyb

--- teacher.py
+++ teacher.py
@@ -3,34 +3,37 @@ import psycopg2
 
 root = Tk()
 root.title("Škola a datáze")
-root.geometry("330x280")
+root.geometry("330x320")
 root.resizable(False,False)
 
 # functions
 def insert_data(name, age, address):
-    entry_name.delete(0, END)
-    entry_age.delete(0, END)
-    entry_address.delete(0, END)
+    if name and age and address: 
+        entry_name.delete(0, END)
+        entry_age.delete(0, END)
+        entry_address.delete(0, END)
 
-    connection = psycopg2.connect(
-                dbname='student',
-                user='postgres',
-                password='admin',
-                host='localhost',
-                port='5432'
-            )
-    
-    cur = connection.cursor()
-    query = ('''INSERT INTO teacher(name, age, address) 
-                VALUES (%s, %s, %s)''')
-    cur.execute(query, (name, age, address))
-    connection.commit()
-    connection.close()
-    display_all()
+        connection = psycopg2.connect(
+                    dbname='student',
+                    user='postgres',
+                    password='admin',
+                    host='localhost',
+                    port='5432'
+                )
+        
+        cur = connection.cursor()
+        query = ('''INSERT INTO teacher(name, age, address) 
+                    VALUES (%s, %s, %s)''')
+        cur.execute(query, (name, age, address))
+        connection.commit()
+        connection.close()
+        show_db()
+    else:
+        listbox.delete(0,END)
+        listbox.insert(0, "zadejte všechny položky")
 
 
 def search_id(id):
-    
     connection = psycopg2.connect(
                 dbname='student',
                 user='postgres',
@@ -51,14 +54,13 @@ def search_id(id):
 
     
 def display_search(data):
-    listbox = Listbox(root, width=20, height=7)
-    listbox.grid(row=7, column=1)
+    listbox.delete(0,END)
     if data:
         listbox.insert(0,("id:",data[0]), ("name:",data[1]), ("age:",data[2]), ("address:",data[3]))
     else:
         listbox.insert(0,"ID nenalezeno")
 
-def display_all():
+def down_db():
     connection = psycopg2.connect(
                 dbname='student',
                 user='postgres',
@@ -71,33 +73,39 @@ def display_all():
     query = ('''SELECT * FROM teacher''')
     cur.execute(query)
     all_teacher = cur.fetchall()
-    listbox = Listbox(root, width=20, height=7)
-    listbox.grid(row=7, column=1)
+    connection.commit()
+    connection.close()
+    return all_teacher
 
-    # vytvoření scrollbar widgetu
-    scrollbar = Scrollbar(root)
-    # umístění scrollbar v okně
-    scrollbar.grid(row=7, column=2, sticky="nsw")
-
-    # připojení Scrollbar k listboxu
-    # scrollbar reaguje na posouvání v listboxu
-    listbox.config(yscrollcommand=scrollbar.set)
-    # listbox reaguje na posouvání scrollbarem
-    scrollbar.config(command=listbox.yview)
-
-    for teacher in all_teacher:
-        listbox.insert(END,teacher)
 
 def error_id():
-     listbox = Listbox(root, width=20, height=7)
-     listbox.grid(row=7, column=1)
+     listbox.delete(0,END)
      listbox.insert(0,"zadejte ID")
 
-# def delete_from_db(to_delete):
-#     connection = psycopg2.connect(
-#         dbname="student",
-#         user="postgres"
-#     )
+def show_db():
+    listbox.delete(0,END)
+    all_teacher = down_db()
+    for teacher in all_teacher:
+            listbox.insert(END,teacher)
+
+def delete_from_db(listbox):
+    selected_indices = listbox.curselection()
+    to_delete = str(listbox.get(selected_indices)[0])
+    connection = psycopg2.connect(
+        dbname="student",
+        user="postgres",
+        password="admin",
+        host="localhost",
+        port="5432"
+    )
+
+    cur = connection.cursor()
+    query = ('''DELETE FROM teacher WHERE id = %s''')
+    cur.execute(query, (to_delete,))
+    connection.commit()
+    connection.close()
+    show_db()
+
     
 # labels a entries
 label_general = Label(root, text="Add data")
@@ -125,7 +133,7 @@ entry_address = Entry(root)
 entry_address.grid(row=3, column=1)
 
 # button
-button = Button(root, text="Add", command=lambda:insert_data(entry_name.get(), entry_age.get(), entry_address.get()))
+button = Button(root, text="Add", command=lambda:insert_data(entry_name.get().strip(), entry_age.get().strip(), entry_address.get().strip()))
 button.grid(row=4, column=1)
 
 ## search section
@@ -136,16 +144,34 @@ label_search.grid(row=5, column=1)
 label_id = Label(root, text="Search by id: ")
 label_id.grid(row=6, column=0)
 
-entry_id=Entry()
+entry_id=Entry(root)
 entry_id.grid(row=6, column=1)
 
 button_search = Button(root, text="search", command=lambda:search_id(entry_id.get()) if entry_id.get().strip() else error_id())
 button_search.grid(row=6, column=3)
 
-button_showall = Button(root, text="show database", command=display_all, height=2, width=7, wraplength=60)
+# vložení listboxu
+listbox = Listbox(root, width=20, height=7)
+listbox.grid(row=7, column=1)
+
+# vytvoření scrollbar widgetu
+scrollbar = Scrollbar(root)
+# umístění scrollbar v okně
+scrollbar.grid(row=7, column=2, sticky="nsw")
+# připojení Scrollbar k listboxu
+# scrollbar reaguje na posouvání v listboxu
+listbox.config(yscrollcommand=scrollbar.set)
+# listbox reaguje na posouvání scrollbarem
+scrollbar.config(command=listbox.yview)
+
+# tlačítko na zobrazení celé databáze
+button_showall = Button(root, text="show database", command=show_db, height=2, width=7, wraplength=60)
 button_showall.grid(row=7, column=3, sticky="e")
 
+# tlačítko na smazání položky z databáze
+button_delete = Button(root, text="delete from db", command=lambda:delete_from_db(listbox))
+button_delete.grid(row=8, column=1)
 
-display_all()
+show_db()
 
 root.mainloop()
